318845fe97e7ab3c119d756d17a3d14b973b6810
Ming-der Wang <mingderwang@operamail.com>
2022-09-26 22:32:21 +0800

    add .d.ts file

--- __tests__/base.spec.ts
+++ __tests__/base.spec.ts
@@ -1,23 +1,23 @@
 import { evaluate, BinaryOperation, BinaryOperators } from "../src";
 
 describe("Simple expression tests", () => {
-    test("Check literal value", () => {
+ test("Check literal value", () => {
         expect(evaluate({ type: "literal", value: 5 })).toBeCloseTo(5);
-    });
-    test("Check addition", () => {
-        let expr = bin("+", 5, 10);
-        expect(evaluate(expr)).toBeCloseTo(15);
-    });
+ });
+ test("Check addition", () => {  
+    const expr = bin("+", 5, 10);
+    expect(evaluate(expr)).toBeCloseTo(15);
+           });
     test("Check subtraction", () => {
-        let expr = bin("-", 5, 10);
+        const expr = bin("-", 5, 10);
         expect(evaluate(expr)).toBeCloseTo(-5);
     });
     test("Check multiplication", () => {
-        let expr = bin("*", 5, 10);
+        const expr = bin("*", 5, 10);
         expect(evaluate(expr)).toBeCloseTo(50);
     });
     test("Check division", () => {
-        let expr = bin("/", 10, 5);
+        const expr = bin("/", 10, 5);
         expect(evaluate(expr)).toBeCloseTo(2);
     });
 });
